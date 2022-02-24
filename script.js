fd9ea9a6fa2b1e10dd5fdcaf04f8a6cadb72f9f5
fatmanevzat <fatmanevzat@outlook.com>
2022-02-25 00:31:46 +0300

    lesson5 bitti

---
+++ script.js
@@ -0,0 +1,91 @@
+/*let name = "Mark";
+
+const logUserName = () => {
+    let isLoggedIn = true;
+    let name = 'Guest User';
+
+    if (isLoggedIn) {
+        let name = 'John Wayne';
+        console.log(name);
+    }
+    console.log(name);
+};
+
+logUserName()
+
+console.log(name)*/
+
+/*let myArr = ["Merhaba" , 5 ,true]
+
+console.log(myArr);
+console.log(myArr[0]);
+console.log(myArr[1]);*/
+
+let myArray = [];
+console.log(myArray);
+
+let newArr = new Array(5);
+console.log(newArr.length);
+console.log(newArr[1]);
+
+let newArray = new Array("merhaba" , "dünya" ,3);
+console.log(newArray);
+console.log(newArray[1]);
+
+newArray[1] = "Jüpiter";
+
+console.log(newArray)
+
+// Split
+
+let lastName = "demir türk";
+
+let newest = lastName.split();
+console.log(newest)
+
+let newest1 = lastName.split("");
+console.log(newest1)
+
+let newest2 = lastName.split(" ");
+console.log(newest2)
+ 
+let letArr = ["mer" , "ha" , 5]
+const constArr = ["mermer" ,"halhal" ,55]
+letArr[1] = "harhar";
+constArr[1] = "hayhay";
+
+constArr[3] = 54;
+
+letArr = ["yeni"]
+
+console.log("letArr==>" , letArr)
+console.log("constArr==>" , constArr)
+
+//Fill
+
+let newArr4 = new Array(2).fill(3)
+console.log(newArr)
+
+let newArray4 = new Array(2)
+
+newArray4[0] = 3
+newArray4[1] = 5
+
+console.log(newArray4)
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
